b474f54dcdac85101d565dd2acc0125b314936b7
anthonype <aescalona@mdsol.com>
2015-03-15 22:53:35 -0400

    fixed failing test

--- HelloWorld/HelloWorldTests/HelloWorldTests.swift
+++ HelloWorld/HelloWorldTests/HelloWorldTests.swift
@@ -23,7 +23,7 @@ class HelloWorldTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(false, "fail")
+        XCTAssert(true, "pass")
     }
     
     func testPerformanceExample() {
